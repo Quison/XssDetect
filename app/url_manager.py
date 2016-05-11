813cd079852750dc13cb0eb85b963d8bcd07c634
duolaa <377726046@qq.com>
2016-05-11 11:30:43 +0800

    add db_sqlite

--- app/url_manager.py
+++ app/url_manager.py
@@ -7,6 +7,11 @@ class UrlManager(object):
 		self.new_urls = set()
 		self.old_urls = set()
 		
+		#list 转换为set，保持原来的程序，转换方式为
+		#
+		#  l = [1,2,3,4,5,6,7,8]
+		#  s = set(l)
+		#  就这样类似强制类型转换。一旦数据量非常大的话，list索引就会很耗时
 
 	def add_new_url(self,url):
 		if url is None:
