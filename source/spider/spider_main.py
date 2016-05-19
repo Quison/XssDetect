e9c7262e98c2afb41a4023a2d7c577bb9c52ff61
Quison <mguichun@foxmail.com>
2016-05-19 22:34:31 +0800

    完善爬虫的编写

--- source/spider/spider_main.py
+++ source/spider/spider_main.py
@@ -5,6 +5,7 @@ import time
 import threading
 import thread
 import wx
+import traceback
 
 import html_downloader
 import html_parser
@@ -18,25 +19,30 @@ class SpiderThread(threading.Thread):
 	爬虫的线程类
 	"""
 
-	def __init__(self, frame, crawl_depth, url_queue, con):
+	def __init__(self, name, frame, crawl_depth, url_queue, con, outputer):
 
 		threading.Thread.__init__(self)
+		# 线程名
+		self.name = name
 		# 初始化参数
 		self.frame = frame
 
 		#初始化爬虫
 		self.downloader = html_downloader.HtmlDownloader()
 		self.parser = html_parser.HtmlParser()
-		self.outputer = html_outputer.HtmlOutputer()
+		self.outputer = outputer
 		self.crawl_depth = crawl_depth
 		self.url_queue = url_queue
 		self.con = con
+
 		self.timeout = 3
 		self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
 
 	def run(self):
 		while 1:
-			if SpiderMain.stoped :
+			#print "空 ",self.url_queue.is_empty()
+			#print "空闲 ",SpiderMain.wait_thread_num
+			if SpiderMain.stoped:
 				break			
 			# 当队列为空时，默认当做还有其他线程正在工作，等待3秒，如果3秒过后还为空则超时退出
 			if self.url_queue.is_empty() and (SpiderMain.thread_num == SpiderMain.wait_thread_num):
@@ -50,21 +56,24 @@ class SpiderThread(threading.Thread):
 					SpiderMain.wait_thread_num -= 1
 					try:
 						spider_url = self.url_queue.get()
-						url_str = spider_url.get_url()
-						url_depth = spider_url.get_depth()
-						html_cont = self.downloader.download(url_str,headers=self.headers,timeout=self.timeout)	
+						html_cont = self.downloader.download(spider_url, headers=self.headers, timeout=self.timeout)	
 						# 将解析得的url放入带爬取链接队列中深度+1
-						new_urls_str = self.parser.parse(url_str,html_cont)
-						# 如果深度如果连接的深度超过了就不加入队列
-						if url_depth + 1 <= self.crawl_depth: 
-							self.url_queue.put(new_urls_str, url_depth+1)
+						spider_url_set = self.parser.parse(spider_url,html_cont)
+						
+						if (spider_url.get_depth() + 1) <= self.crawl_depth:
+							# 如果深度如果连接的深度超过了就不加入队列
+							self.url_queue.put(spider_url_set)
+							# 将数据写到数据库（注没过滤的后面调整）
+							self.outputer.write_date(spider_url_set)
+							# 唤醒因为队列为空而等待的线程
 							self.notifyAll()
+
 						if self.frame != None:
-							wx.CallAfter(self.frame.print_on_spider_grid, [self.getName(), url_str, spider_url.get_method(), url_depth] )
-						#else:
-						print self.getName() +" url:" + url_str + " depth:" + str(url_depth)
-						self.outputer.collect_urls(new_urls_str)
-					except Exception:
+							wx.CallAfter(self.frame.print_on_spider_grid, [self.getName(), spider_url.get_url(), spider_url.get_method(), spider_url.get_depth()] )
+						else:
+							print self.getName() +" url:" + spider_url.get_url() + " depth:" + str(spider_url.get_depth()) + " method:" + spider_url.get_method()
+					except Exception, e:
+						print traceback.format_exc()
 						print "爬虫错误！"
 					finally:
 						if SpiderMain.wait_thread_num < SpiderMain.thread_num:
@@ -99,6 +108,8 @@ class SpiderMain:
 		self.con = threading.Condition()
 
 		self.url_queue = UrlQueue(root_url)
+		self.outputer = html_outputer.HtmlOutputer()
+
 		self.threads = []
 		
 	def crawling(self):
@@ -106,13 +117,12 @@ class SpiderMain:
 		初始化线程爬取
 		"""
 		for x in range(SpiderMain.thread_num):
-			t = SpiderThread(self.frame, self.crawl_depth, self.url_queue, self.con)
+			t = SpiderThread("Thread-"+str(x+1), self.frame, self.crawl_depth, self.url_queue, self.con, self.outputer)
 			self.threads.append(t)
 			t.start()
 
 		# 启动一个线程检测所有线程，当所有完成工作后提示
 		thread.start_new_thread(self.check_done,())
-
 
 	def stop(self):
 		"""
@@ -137,10 +147,17 @@ class SpiderMain:
 		while 1:
 			if self.all_is_done():
 				break
-		self.frame.confirm_dialog(u"爬取完成！")
+		if self.frame is not None:
+			self.frame.confirm_dialog(u"爬取完成！")
+			if self.frame.start_crawling_button.GetLabel() == u"暂停爬取":
+				self.frame.start_crawling_button.SetLabel(u"开始爬取")
+
+		# 睡眠三秒再关闭数据库连接，不然有些线程没写完会引发异常
+		time.sleep(3)
+		self.outputer.close_sql_worker()
 
 if __name__ == '__main__':
-	root_url = "http://www.cnblogs.com/hongten/p/hongten_python_sqlite3.html"
+	root_url = "https://www.processon.com/login"
 	crawl_depth = 2
 	thread_num = 5
 	con = threading.Condition()
--- source/spider/url_queue.py
+++ source/spider/url_queue.py
@@ -17,7 +17,6 @@ class UrlQueue(object):
 		self.url_filter = set()
 		# 已爬取过的url
 		self.old_urls = set()
-
 		# 初始化
 		self.url_queue.put(SpiderUrl(root_url, 0))
 		self.domain = re.match(r"^(http(s)?://)?([\w-]+\.)+[\w-]+/?",root_url,re.M|re.I).group()
@@ -30,23 +29,23 @@ class UrlQueue(object):
 			return
 
 		url_str = spider_url.get_url()
+		url_depth = spider_url.get_depth()
 		new_domain = re.match(r"^(http(s)?://)?([\w-]+\.)+[\w-]+/?",url_str,re.M|re.I)
-		if url_str not in self.url_filter and new_domain and (new_domain.group()==self.domain):
+		if (url_str not in self.url_filter) and new_domain and (new_domain.group()==self.domain):
 			# 将url添加到带待爬取的url队列
 			self.url_queue.put(spider_url)
 			# 同时将url添加到过滤器
 			self.url_filter.add(url_str)
 
-
-	def put(self, urls_str, depth):
+	def put(self, spider_urls):
 		"""
 		批量添加新链接
 		"""
-		if urls_str is None or len(urls_str) == 0:
+		if spider_urls is None:
 			return
 
-		for url_str in urls_str:
-			self.__add_new_url(SpiderUrl(url_str, depth))
+		for spider_url in spider_urls:
+			self.__add_new_url(spider_url)
 
 	def is_empty(self):
 		"""
